70e25bca00c801e609c4a71ed1f6fbd2689e7fab
wangcongying <wangcongying@n-wangcongying.cattsoft.com>
2016-12-07 21:53:00 +0800

    最长递增子序列-DP算法

---
+++ leetcode/DP/LIS.py
@@ -0,0 +1,39 @@
+'''
+Created on 2016年12月7日
+
+@author: wangcongying
+'''
+#-*-coding:utf-8-*-
+#最长递增子序列:5, 3, 4, 8, 6, 7, 9 的最长递增子序列是3,4,6,7,9 所以得到的长度是5
+
+def lis(nums):
+    res = 1
+    lenOfNums = len(nums)
+    d = [0]*lenOfNums
+    for i in range(lenOfNums):
+        d[i] = 1
+        for j in range(i):
+            if nums[j]<nums[i] and d[j]+1>d[i]:
+                d[i] = d[j]+1
+        if d[i]>res:
+            res = d[i]
+    print(d)
+    return res
+    
+    
+            
+            
+            
+            
+    
+    return res
+
+if __name__ == "__main__":
+    nums = [5, 3, 4, 8, 6, 7, 9]
+    print(lis(nums))
+
+
+
+
+
+
